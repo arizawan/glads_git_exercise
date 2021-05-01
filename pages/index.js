08120a472418bcd097d0baa21effcc3fb6a69fb1
Ahmed Rizawan <ahm.rizawan@gmail.com>
2021-05-01 20:03:40 +0600

    First Init

--- pages/index.js
+++ pages/index.js
@@ -4,9 +4,22 @@ export default function IndexPage() {
   return (
     <div>
       Hello World.{' '}
-      <Link href="/about">
-        <a>About</a>
-      </Link>
+      <ul>
+        <li>
+          <a>
+          <Link href="/about">
+            <a>About</a>
+          </Link>
+          </a>
+        </li>
+        <li>
+          <a>
+          <Link href="/contact">
+            <a>Contact</a>
+          </Link>
+          </a>
+        </li>
+      </ul>
     </div>
   )
 }
